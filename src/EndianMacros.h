1e1128ee644fe218010994856c79ca5ed98632cd
Jaak Ristioja <jaak.ristioja@cyber.ee>
2015-01-22 16:08:18 +0200

    Moved some endian macros to SharemindCHeaders.

--- src/EndianMacros.h
+++ src/EndianMacros.h
@@ -20,43 +20,8 @@
 #ifndef SHAREMIND_ENDIAN_MACROS_H
 #define SHAREMIND_ENDIAN_MACROS_H
 
-#ifdef __MACH__
-
-#include <libkern/OSByteOrder.h>
-#define htobe16(x) (OSSwapHostToBigInt16((x)))
-#define htole16(x) (OSSwapHostToLittleInt16((x)))
-#define be16toh(x) (OSSwapBigToHostInt16((x)))
-#define le16toh(x) (OSSwapLittleToHostInt16((x)))
-#define htobe32(x) (OSSwapHostToBigInt32((x)))
-#define htole32(x) (OSSwapHostToLittleInt32((x)))
-#define be32toh(x) (OSSwapBigToHostInt32((x)))
-#define le32toh(x) (OSSwapLittleToHostInt32((x)))
-#define htobe64(x) (OSSwapHostToBigInt64((x)))
-#define htole64(x) (OSSwapHostToLittleInt64((x)))
-#define be64toh(x) (OSSwapBigToHostInt64((x)))
-#define le64toh(x) (OSSwapLittleToHostInt64((x)))
-
-#elif __FreeBSD__
-
-#ifndef _BSD_SOURCE
-#define _BSD_SOURCE
-#endif
-
-#include <sys/endian.h>
-
-#else
-
-#ifndef _BSD_SOURCE
-#define _BSD_SOURCE
-#endif
-#include <endian.h>
-
-#endif
-
-#ifdef __cplusplus
-
 #include <cstdint>
-
+#include <sharemind/endian.h>
 
 namespace sharemind {
 
@@ -90,7 +55,5 @@ template <> inline int32_t littleEndianToHost(const int32_t v) { return le32toh(
 template <> inline int64_t littleEndianToHost(const int64_t v) { return le64toh(v); }
 
 } /* namespace sharemind { */
-
-#endif
 
 #endif /* SHAREMIND_ENDIAN_MACROS_H */
