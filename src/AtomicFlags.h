0e9088aa1eac26d8e34b15e045bbae938a18e0b3
Jaak Ristioja <jaak.ristioja@cyber.ee>
2015-04-14 13:52:55 +0300

    AtomicFlags enhancements.

--- src/AtomicFlags.h
+++ src/AtomicFlags.h
@@ -22,47 +22,187 @@
 
 #include <atomic>
 #include <type_traits>
+#include "Flags.h"
 
 
 namespace sharemind {
 namespace Detail {
 
-template <typename FlagEnum,
-          typename =
-                typename std::enable_if<std::is_enum<FlagEnum>::value>::type>
-class AtomicFlags:
-        public std::atomic<typename std::underlying_type<FlagEnum>::type>
-{
-
-public: /* Types: */
-
-    using Underlying = typename std::underlying_type<FlagEnum>::type;
-
-private: /* Types: */
-
-    using Base = std::atomic<Underlying>;
+template <typename T, bool = std::is_integral<T>::value>
+class AtomicFlags {
 
 public: /* Methods: */
 
     AtomicFlags() noexcept {}
-    constexpr AtomicFlags(Underlying const value) noexcept : Base{value} {}
+    constexpr explicit AtomicFlags(T const value) noexcept : m_flags{value} {}
 
-    Underlying setFlags(Underlying const flags,
-                        std::memory_order const memoryOrder =
-                                std::memory_order_seq_cst) noexcept
-    { return this->fetch_or(flags, memoryOrder); }
+    Flags<T> load(std::memory_order const memoryOrder =
+                          std::memory_order_seq_cst) noexcept
+    { return m_flags.load(memoryOrder); }
 
-    Underlying unsetFlags(Underlying const flags,
+    void store(T const flags,
+               std::memory_order const memoryOrder =
+                       std::memory_order_seq_cst) noexcept
+    { return m_flags.store(flags, memoryOrder); }
+
+    inline Flags<T> exchange(T const flags,
+                             std::memory_order const memoryOrder =
+                                     std::memory_order_seq_cst) noexcept
+    { return m_flags.exchange(flags, memoryOrder); }
+
+    inline bool compareExchangeStrong(T & expected,
+                                      T const flags,
+                                      std::memory_order const memoryOrder =
+                                              std::memory_order_seq_cst)
+            noexcept
+    { return m_flags.compare_exchange_strong(expected, flags, memoryOrder); }
+
+    inline bool compareExchangeStrong(T & expected,
+                                      T const flags,
+                                      std::memory_order const success,
+                                      std::memory_order const failure)
+            noexcept
+    {
+        return m_flags.compare_exchange_strong(expected,
+                                               flags,
+                                               success,
+                                               failure);
+    }
+
+    inline bool compareExchangeStrong(Flags<T> & expected,
+                                      T const flags,
+                                      std::memory_order const memoryOrder =
+                                              std::memory_order_seq_cst)
+            noexcept
+    {
+        return m_flags.compare_exchange_strong(expected.m_flags,
+                                               flags,
+                                               memoryOrder);
+    }
+
+    inline bool compareExchangeStrong(Flags<T> & expected,
+                                      T const flags,
+                                      std::memory_order const success,
+                                      std::memory_order const failure)
+            noexcept
+    {
+        return m_flags.compare_exchange_strong(expected.m_flags,
+                                               flags,
+                                               success,
+                                               failure);
+    }
+
+    inline bool compareExchangeWeak(T & expected,
+                                    T const flags,
+                                    std::memory_order const memoryOrder =
+                                            std::memory_order_seq_cst)
+            noexcept
+    { return m_flags.compare_exchange_weak(expected, flags, memoryOrder); }
+
+    inline bool compareExchangeWeak(T & expected,
+                                    T const flags,
+                                    std::memory_order const success,
+                                    std::memory_order const failure)
+            noexcept
+    { return m_flags.compare_exchange_weak(expected, flags, success, failure); }
+
+    inline bool compareExchangeWeak(Flags<T> & expected,
+                                    T const flags,
+                                    std::memory_order const memoryOrder =
+                                            std::memory_order_seq_cst)
+            noexcept
+    {
+        return m_flags.compare_exchange_weak(expected.m_flags,
+                                             flags,
+                                             memoryOrder);
+    }
+
+    inline bool compareExchangeWeak(Flags<T> & expected,
+                                    T const flags,
+                                    std::memory_order const success,
+                                    std::memory_order const failure)
+            noexcept
+    {
+        return m_flags.compare_exchange_weak(expected.m_flags,
+                                             flags,
+                                             success,
+                                             failure);
+    }
+
+    void setFlags(std::memory_order const memoryOrder =
+                          std::memory_order_seq_cst) noexcept
+    { return m_flags.store(~static_cast<T>(0), memoryOrder); }
+
+    Flags<T> fetchSetFlags(std::memory_order const memoryOrder =
+                                   std::memory_order_seq_cst) noexcept
+    { return exchange(~static_cast<T>(0), memoryOrder); }
+
+    Flags<T> fetchSetFlags(T const flags,
+                           std::memory_order const memoryOrder =
+                                   std::memory_order_seq_cst) noexcept
+    { return m_flags.fetch_or(flags, memoryOrder); }
+
+    void unsetFlags(std::memory_order const memoryOrder =
+                            std::memory_order_seq_cst) noexcept
+    { return m_flags.store(static_cast<T>(0), memoryOrder); }
+
+    Flags<T> fetchUnsetFlags(std::memory_order const memoryOrder =
+                                     std::memory_order_seq_cst) noexcept
+    { return exchange(static_cast<T>(0), memoryOrder); }
+
+    Flags<T> fetchUnsetFlags(T const flags,
+                             std::memory_order const memoryOrder =
+                                     std::memory_order_seq_cst) noexcept
+    { return m_flags.fetch_and(~flags, memoryOrder); }
+
+    Flags<T> fetchToggleFlags(T const flags,
+                              std::memory_order const memoryOrder =
+                                      std::memory_order_seq_cst) noexcept
+    { return m_flags.fetch_xor(flags, memoryOrder); }
+
+    bool has(T const flags,
+             T const of,
+             std::memory_order const memoryOrder =
+                     std::memory_order_seq_cst) const noexcept
+    { return load(memoryOrder).has(flags, of); }
+
+    inline bool hasAnyOf(T const flags,
+                         std::memory_order const memoryOrder =
+                                 std::memory_order_seq_cst) const noexcept
+    { return load(memoryOrder).hasAnyOf(flags); }
+
+    inline bool hasAllOf(T const flags,
+                         std::memory_order const memoryOrder =
+                                 std::memory_order_seq_cst) const noexcept
+    { return load(memoryOrder).hasAllOf(flags); }
+
+    inline bool hasNoneOf(T const flags,
                           std::memory_order const memoryOrder =
-                                  std::memory_order_seq_cst) noexcept
-    { return this->fetch_and(~flags, memoryOrder); }
+                                  std::memory_order_seq_cst) const noexcept
+    { return load(memoryOrder).hasNoneOf(flags); }
+
+private: /* Fields: */
+
+    std::atomic<T> m_flags;
+
+};
+
+template <typename T>
+class AtomicFlags<T, false>:
+        public AtomicFlags<typename std::underlying_type<T>::type>
+{
+
+public: /* Methods: */
+
+    using AtomicFlags<typename std::underlying_type<T>::type>::AtomicFlags;
+    using AtomicFlags<typename std::underlying_type<T>::type>::operator=;
 
 };
 
 } /* namespace Detail { */
 
-template <typename FlagsEnum>
-using AtomicFlags = Detail::AtomicFlags<FlagsEnum>;
+template <typename EnumOrIntegral>
+using AtomicFlags = Detail::AtomicFlags<EnumOrIntegral>;
 
 } /* namespace Sharemind { */
 
