sharemind-sdk/cxxheaders

commit f1ef0aa076cb45f3d689f9ac5eef7f2e74d4d548

CircBufferSCSP.h: Reordered declarations to fix compile error with clang.

src/CircBufferSCSP.h
@@ -400,24 +400,6 @@ public: /* Methods: */
     inline size_t write(const T * data, const size_t size) noexcept
     { return operate<WriteActions>(data, size); }
 
-    /**
-     * \brief Uses the given actor to write to the buffer.
-     * \param actor The actor producing the input.
-     * \returns the number of elements written.
-    */
-    template <typename InputProducerActor>
-    inline size_t write(InputProducerActor && inputProducerActor)
-            noexcept(noexcept(
-                        std::declval<Self &>().template operate<
-                                typename Self::WriteActions,
-                                InputProducerActor>(
-                             std::forward<InputProducerActor>(
-                                 inputProducerActor))))
-    {
-        return operate<WriteActions, InputProducerActor>(
-                    std::forward<InputProducerActor>(inputProducerActor));
-    }
-
 
     /***************************************************************************
      * Procedures for consumer */
@@ -561,24 +543,6 @@ public: /* Methods: */
     */
     inline size_t read(T * const buffer, const size_t size) noexcept
     { return operate<ReadActions>(buffer, size); }
-
-    /**
-     * \brief Uses the given actor to read from the buffer.
-     * \param actor The actor consuming the input.
-     * \returns the number of elements read.
-    */
-    template <typename OutputConsumerActor>
-    inline size_t read(OutputConsumerActor && outputConsumerActor)
-            noexcept(noexcept(
-                         std::declval<Self &>().template operate<
-                                Self::ReadActions,
-                                OutputConsumerActor>(
-                             std::forward<OutputConsumerActor>(
-                                 outputConsumerActor))))
-    {
-        return operate<ReadActions, OutputConsumerActor>(
-                    std::forward<OutputConsumerActor>(outputConsumerActor));
-    }
 
 protected: /* Methods: */
 
@@ -779,6 +743,44 @@ private: /* Types: */
     #if defined(SHAREMIND_GCC_VERSION) && (SHAREMIND_GCC_VERSION < 40800)
     #undef Self
     #endif
+
+public: /* Methods */
+
+    /**
+     * \brief Uses the given actor to write to the buffer.
+     * \param actor The actor producing the input.
+     * \returns the number of elements written.
+    */
+    template <typename InputProducerActor>
+    inline size_t write(InputProducerActor && inputProducerActor)
+            noexcept(noexcept(
+                        std::declval<Self &>().template operate<
+                                typename Self::WriteActions,
+                                InputProducerActor>(
+                             std::forward<InputProducerActor>(
+                                 inputProducerActor))))
+    {
+        return operate<WriteActions, InputProducerActor>(
+                    std::forward<InputProducerActor>(inputProducerActor));
+    }
+
+    /**
+     * \brief Uses the given actor to read from the buffer.
+     * \param actor The actor consuming the input.
+     * \returns the number of elements read.
+    */
+    template <typename OutputConsumerActor>
+    inline size_t read(OutputConsumerActor && outputConsumerActor)
+            noexcept(noexcept(
+                         std::declval<Self &>().template operate<
+                                Self::ReadActions,
+                                OutputConsumerActor>(
+                             std::forward<OutputConsumerActor>(
+                                 outputConsumerActor))))
+    {
+        return operate<ReadActions, OutputConsumerActor>(
+                    std::forward<OutputConsumerActor>(outputConsumerActor));
+    }
 
 private: /* Fields :*/
 

commit cfc27591d24bc1ed60ba1eb86cb2ffe093e70cb6

CircBufferSCSP: Fixed compilation with GCC < 4.8.
Was broken by f1ef0aa076cb45f3d689f9ac5eef7f2e74d4d548 with message
  "CircBufferSCSP.h: Reordered declarations to fix compile error with clang."

src/CircBufferSCSP.h
@@ -740,10 +740,6 @@ private: /* Types: */
                               &Self::haveReadRetPair,
                               &Self::haveReadRetUbe> ReadActions;
 
-    #if defined(SHAREMIND_GCC_VERSION) && (SHAREMIND_GCC_VERSION < 40800)
-    #undef Self
-    #endif
-
 public: /* Methods */
 
     /**
@@ -781,6 +777,10 @@ public: /* Methods */
         return operate<ReadActions, OutputConsumerActor>(
                     std::forward<OutputConsumerActor>(outputConsumerActor));
     }
+
+    #if defined(SHAREMIND_GCC_VERSION) && (SHAREMIND_GCC_VERSION < 40800)
+    #undef Self
+    #endif
 
 private: /* Fields :*/
 