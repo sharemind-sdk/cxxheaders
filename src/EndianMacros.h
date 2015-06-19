f8e0cd1fbf407f64dc9b2ebcc1d7415130263c09
Jaak Ristioja <jaak.ristioja@cyber.ee>
2015-06-19 13:54:45 +0300

    EndianMacros.h: Restored the signed versions of functions.

--- src/EndianMacros.h
+++ src/EndianMacros.h
@@ -30,29 +30,53 @@ inline uint8_t  hostToBigEndian(uint8_t  const v) noexcept { return v; }
 inline uint16_t hostToBigEndian(uint16_t const v) noexcept { return htobe16(v); }
 inline uint32_t hostToBigEndian(uint32_t const v) noexcept { return htobe32(v); }
 inline uint64_t hostToBigEndian(uint64_t const v) noexcept { return htobe64(v); }
+inline int8_t  hostToBigEndian(int8_t  const v) noexcept { return v; }
+inline int16_t hostToBigEndian(int16_t const v) noexcept { return htobe16(v); }
+inline int32_t hostToBigEndian(int32_t const v) noexcept { return htobe32(v); }
+inline int64_t hostToBigEndian(int64_t const v) noexcept { return htobe64(v); }
 
 inline uint8_t  bigEndianToHost(uint8_t  const v) noexcept { return v; }
 inline uint16_t bigEndianToHost(uint16_t const v) noexcept { return be16toh(v); }
 inline uint32_t bigEndianToHost(uint32_t const v) noexcept { return be32toh(v); }
 inline uint64_t bigEndianToHost(uint64_t const v) noexcept { return be64toh(v); }
+inline int8_t  bigEndianToHost(int8_t  const v) noexcept { return v; }
+inline int16_t bigEndianToHost(int16_t const v) noexcept { return be16toh(v); }
+inline int32_t bigEndianToHost(int32_t const v) noexcept { return be32toh(v); }
+inline int64_t bigEndianToHost(int64_t const v) noexcept { return be64toh(v); }
 
 inline uint8_t  hostToLittleEndian(uint8_t  const v) noexcept { return v; }
 inline uint16_t hostToLittleEndian(uint16_t const v) noexcept { return htole16(v); }
 inline uint32_t hostToLittleEndian(uint32_t const v) noexcept { return htole32(v); }
 inline uint64_t hostToLittleEndian(uint64_t const v) noexcept { return htole64(v); }
+inline int8_t  hostToLittleEndian(int8_t  const v) noexcept { return v; }
+inline int16_t hostToLittleEndian(int16_t const v) noexcept { return htole16(v); }
+inline int32_t hostToLittleEndian(int32_t const v) noexcept { return htole32(v); }
+inline int64_t hostToLittleEndian(int64_t const v) noexcept { return htole64(v); }
 inline uint8_t  hostToNetOrder(uint8_t  const v) noexcept { return v; }
 inline uint16_t hostToNetOrder(uint16_t const v) noexcept { return htole16(v); }
 inline uint32_t hostToNetOrder(uint32_t const v) noexcept { return htole32(v); }
 inline uint64_t hostToNetOrder(uint64_t const v) noexcept { return htole64(v); }
+inline int8_t  hostToNetOrder(int8_t  const v) noexcept { return v; }
+inline int16_t hostToNetOrder(int16_t const v) noexcept { return htole16(v); }
+inline int32_t hostToNetOrder(int32_t const v) noexcept { return htole32(v); }
+inline int64_t hostToNetOrder(int64_t const v) noexcept { return htole64(v); }
 
 inline uint8_t  littleEndianToHost(uint8_t  const v) noexcept { return v; }
 inline uint16_t littleEndianToHost(uint16_t const v) noexcept { return le16toh(v); }
 inline uint32_t littleEndianToHost(uint32_t const v) noexcept { return le32toh(v); }
 inline uint64_t littleEndianToHost(uint64_t const v) noexcept { return le64toh(v); }
+inline int8_t  littleEndianToHost(int8_t  const v) noexcept { return v; }
+inline int16_t littleEndianToHost(int16_t const v) noexcept { return le16toh(v); }
+inline int32_t littleEndianToHost(int32_t const v) noexcept { return le32toh(v); }
+inline int64_t littleEndianToHost(int64_t const v) noexcept { return le64toh(v); }
 inline uint8_t  netToHostOrder(uint8_t  const v) noexcept { return v; }
 inline uint16_t netToHostOrder(uint16_t const v) noexcept { return le16toh(v); }
 inline uint32_t netToHostOrder(uint32_t const v) noexcept { return le32toh(v); }
 inline uint64_t netToHostOrder(uint64_t const v) noexcept { return le64toh(v); }
+inline int8_t  netToHostOrder(int8_t  const v) noexcept { return v; }
+inline int16_t netToHostOrder(int16_t const v) noexcept { return le16toh(v); }
+inline int32_t netToHostOrder(int32_t const v) noexcept { return le32toh(v); }
+inline int64_t netToHostOrder(int64_t const v) noexcept { return le64toh(v); }
 
 } /* namespace sharemind { */
 
