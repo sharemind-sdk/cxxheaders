ce1c5a1f4d0d6a876d05c16123aca28e5e0013a7
Jaak Ristioja <jaak.ristioja@cyber.ee>
2016-01-21 12:36:39 +0200

    Implemented TemplateInverseCondAppend(Type)?

---
+++ src/TemplateInverseCondAppend.h
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2015 Cybernetica
+ *
+ * Research/Commercial License Usage
+ * Licensees holding a valid Research License or Commercial License
+ * for the Software may use this file according to the written
+ * agreement between you and Cybernetica.
+ *
+ * GNU General Public License Usage
+ * Alternatively, this file may be used under the terms of the GNU
+ * General Public License version 3.0 as published by the Free Software
+ * Foundation and appearing in the file LICENSE.GPL included in the
+ * packaging of this file.  Please review the following information to
+ * ensure the GNU General Public License version 3.0 requirements will be
+ * met: http://www.gnu.org/copyleft/gpl-3.0.html.
+ *
+ * For further information, please contact us at sharemind@cyber.ee.
+ */
+
+#ifndef SHAREMIND_TEMPLATEINVERSECONDAPPEND_H
+#define SHAREMIND_TEMPLATEINVERSECONDAPPEND_H
+
+#include <type_traits>
+#include "TemplateAppendOne.h"
+
+
+namespace sharemind {
+
+template <typename T,
+          template <T> class Pred,
+          template <T ...> class Tmpl,
+          typename TmplInstance,
+          T ...>
+struct TemplateInverseCondAppend;
+
+template <typename T,
+          template <T> class Pred,
+          template <T ...> class Tmpl,
+          typename TmplInstance>
+struct TemplateInverseCondAppend<T, Pred, Tmpl, TmplInstance>
+{ using type = TmplInstance; };
+
+template <typename T,
+          template <T> class Pred,
+          template <T ...> class Tmpl,
+          typename TmplInstance,
+          T v,
+          T ... vs>
+struct TemplateInverseCondAppend<T, Pred, Tmpl, TmplInstance, v, vs...> {
+    using type =
+            typename ::std::conditional<
+                Pred<v>::value,
+                typename TemplateInverseCondAppend<T, Pred, Tmpl, TmplInstance, vs...>
+                        ::type,
+                typename TemplateInverseCondAppend<
+                    T,
+                    Pred,
+                    Tmpl,
+                    typename TemplateAppendOne<T, Tmpl, TmplInstance, v>::type,
+                    vs...
+                >::type
+            >::type;
+};
+
+} /* namespace Sharemind { */
+
+#endif /* SHAREMIND_TEMPLATEINVERSECONDAPPEND_H */
---
+++ src/TemplateInverseCondAppendType.h
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2015 Cybernetica
+ *
+ * Research/Commercial License Usage
+ * Licensees holding a valid Research License or Commercial License
+ * for the Software may use this file according to the written
+ * agreement between you and Cybernetica.
+ *
+ * GNU General Public License Usage
+ * Alternatively, this file may be used under the terms of the GNU
+ * General Public License version 3.0 as published by the Free Software
+ * Foundation and appearing in the file LICENSE.GPL included in the
+ * packaging of this file.  Please review the following information to
+ * ensure the GNU General Public License version 3.0 requirements will be
+ * met: http://www.gnu.org/copyleft/gpl-3.0.html.
+ *
+ * For further information, please contact us at sharemind@cyber.ee.
+ */
+
+#ifndef SHAREMIND_TEMPLATEINVERSECONDAPPENDTYPE_H
+#define SHAREMIND_TEMPLATEINVERSECONDAPPENDTYPE_H
+
+#include <type_traits>
+#include "TemplateAppendOneType.h"
+
+
+namespace sharemind {
+
+template <template <typename T> class Pred,
+          template <typename ...> class Tmpl,
+          typename TmplInstance,
+          typename ...>
+struct TemplateInverseCondAppendType;
+
+template <template <typename T> class Pred,
+          template <typename ...> class Tmpl,
+          typename TmplInstance>
+struct TemplateInverseCondAppendType<Pred, Tmpl, TmplInstance>
+{ using type = TmplInstance; };
+
+template <template <typename T> class Pred,
+          template <typename ...> class Tmpl,
+          typename TmplInstance,
+          typename T,
+          typename ... Ts>
+struct TemplateInverseCondAppendType<Pred, Tmpl, TmplInstance, T, Ts...> {
+    using type =
+            typename ::std::conditional<
+                Pred<T>::value,
+                typename TemplateInverseCondAppendType<
+                    Pred,
+                    Tmpl,
+                    typename TemplateAppendOneType<Tmpl, TmplInstance, T>::type,
+                    Ts...
+                >::type,
+                typename TemplateInverseCondAppendType<Pred, Tmpl, TmplInstance, Ts...>
+                        ::type
+            >::type;
+};
+
+} /* namespace Sharemind { */
+
+#endif /* SHAREMIND_TEMPLATEINVERSECONDAPPENDTYPE_H */
