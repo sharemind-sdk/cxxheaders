d29d511e555967602503f5e5d28f1639efc2d832
Jaak Ristioja <jaak.ristioja@cyber.ee>
2016-02-09 14:21:23 +0200

    Implemented MaybeDynamicPackingInfo.

---
+++ tests/TestMaybeDynamicPackingInfo.cpp
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2015 Cybernetica
+ *
+ * Research/Commercial License Usage
+ * Licensees holding a valid Research License or Commercial License
+ * for the Software may use this file according to the written
+ * agreement between you and Cybernetica.
+ *
+ * GNU General Public License Usage
+ * Alternatively, this file may be used under the terms of the GNU
+ * General Public License version 3.0 as published by the Free Software
+ * Foundation and appearing in the file LICENSE.GPL included in the
+ * packaging of this file.  Please review the following information to
+ * ensure the GNU General Public License version 3.0 requirements will be
+ * met: http://www.gnu.org/copyleft/gpl-3.0.html.
+ *
+ * For further information, please contact us at sharemind@cyber.ee.
+ */
+
+#include "../src/MaybeDynamicPackingInfo.h"
+
+#include <type_traits>
+
+
+using namespace sharemind;
+using R = DynamicFieldPlaceholder;
+template <typename T> using A = DynamicVectorFieldPlaceholder<T>;
+template <typename ... Ts> using D = DynamicPackingInfo<Ts...>;
+template <typename ... Ts> using M = MaybeDynamicPackingInfo<Ts...>;
+template <typename ... Ts> using S = PackingInfo<Ts...>;
+
+template <template <typename ...> class T, typename ... Ts>
+constexpr bool test() noexcept
+{ return std::is_same<M<Ts...>, T<Ts...> >::value; }
+
+template <typename ... Ts> constexpr bool testStatic() noexcept
+{ return test<S, Ts...>(); }
+
+template <typename ... Ts> constexpr bool testDynamic() noexcept
+{ return test<D, Ts...>(); }
+
+template <typename ...> struct T {};
+static_assert(
+        std::is_same<
+            MaybeDynamicPackingInfoFromTemplate_t<T<A<int>, char> >,
+            MaybeDynamicPackingInfo<A<int>, char>
+        >::value, "");
+
+int main() {
+    static_assert(testStatic<>(), "");
+    static_assert(testStatic<int>(), "");
+    static_assert(testStatic<int, int>(), "");
+    static_assert(testStatic<int, int, int>(), "");
+    static_assert(testDynamic<A<int> >(), "");
+    static_assert(testDynamic<R>(), "");
+    static_assert(testDynamic<int, A<int> >(), "");
+    static_assert(testDynamic<int, R>(), "");
+    static_assert(testDynamic<A<int>, int>(), "");
+    static_assert(testDynamic<R, int>(), "");
+    static_assert(testDynamic<int, A<int>, int>(), "");
+    static_assert(testDynamic<int, R, int>(), "");
+}
