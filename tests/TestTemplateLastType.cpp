7c8f9360b5e2eb582f69e8a93611b90fb3bddb8b
Jaak Ristioja <jaak.ristioja@cyber.ee>
2016-02-09 15:16:21 +0200

    Implemented TemplateFirstType and TemplateLastType.

---
+++ tests/TestTemplateLastType.cpp
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2015 Cybernetica
+ *
+ * Research/Commercial License Usage
+ * Licensees holding a valid Research License or Commercial License
+ * for the Software may use this file according to the written
+ * agreement between you and Cybernetica.
+ *
+ * GNU General Public License Usage
+ * Alternatively, this file may be used under the terms of the GNU
+ * General Public License version 3.0 as published by the Free Software
+ * Foundation and appearing in the file LICENSE.GPL included in the
+ * packaging of this file.  Please review the following information to
+ * ensure the GNU General Public License version 3.0 requirements will be
+ * met: http://www.gnu.org/copyleft/gpl-3.0.html.
+ *
+ * For further information, please contact us at sharemind@cyber.ee.
+ */
+
+#include "../src/TemplateLastType.h"
+
+#include <type_traits>
+
+
+int main() {
+    using namespace sharemind;
+    static_assert(std::is_same<TemplateLastType<char>, char>::value, "");
+    static_assert(std::is_same<TemplateLastType<float, int, char>, char>::value,
+                  "");
+}
