00be044ac157129d64479f11ab77c398b049cc12
Jaak Ristioja <jaak.ristioja@cyber.ee>
2016-03-22 15:30:30 +0200

    Implemented SharedResourceMap.

---
+++ src/SharedResourceMap.h
@@ -0,0 +1,134 @@
+/*
+ * Copyright (C) 2015 Cybernetica
+ *
+ * Research/Commercial License Usage
+ * Licensees holding a valid Research License or Commercial License
+ * for the Software may use this file according to the written
+ * agreement between you and Cybernetica.
+ *
+ * GNU General Public License Usage
+ * Alternatively, this file may be used under the terms of the GNU
+ * General Public License version 3.0 as published by the Free Software
+ * Foundation and appearing in the file LICENSE.GPL included in the
+ * packaging of this file.  Please review the following information to
+ * ensure the GNU General Public License version 3.0 requirements will be
+ * met: http://www.gnu.org/copyleft/gpl-3.0.html.
+ *
+ * For further information, please contact us at sharemind@cyber.ee.
+ */
+
+#ifndef SHAREMIND_SHAREDRESOURCEMAP_H
+#define SHAREMIND_SHAREDRESOURCEMAP_H
+
+#include <cassert>
+#include <condition_variable>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <utility>
+
+
+namespace sharemind {
+
+template <typename K, typename T>
+class SharedResourceMap {
+
+private: /* Types: */
+
+    struct DefaultConstructor_ { T * operator()() const { return new T; } };
+
+    struct ValueObj_ {
+        std::weak_ptr<T> weakPtr;
+        std::unique_ptr<T> realPtr;
+    };
+
+    using Map_ = std::map<K, ValueObj_>;
+
+public: /* Types: */
+
+    using type = SharedResourceMap<K, T>;
+
+public: /* Methods: */
+
+    template <typename F>
+    void forEach(F f) const {
+        std::unique_lock<std::mutex> lock(m_mutex);
+        for (auto const & v : m_data)
+            if (auto ptr = v.second.weakPtr.lock())
+                f(v.first, std::move(ptr));
+    }
+
+    void waitForEmpty() const noexcept {
+        std::unique_lock<std::mutex> lock(m_mutex);
+        m_cond.wait(lock, [this]() noexcept { return m_data.empty(); });
+    }
+
+    auto empty() const noexcept(noexcept(std::declval<Map_ const &>().empty()))
+            -> decltype(std::declval<Map_ const &>().empty())
+    {
+        std::lock_guard<std::mutex> const guard(m_mutex);
+        return m_data.empty();
+    }
+
+    auto size() const noexcept(noexcept(std::declval<Map_ const &>().size()))
+            -> decltype(std::declval<Map_ const &>().size())
+    {
+        std::lock_guard<std::mutex> const guard(m_mutex);
+        return m_data.size();
+    }
+
+    template <typename Constructor = DefaultConstructor_>
+    std::shared_ptr<T> getResource(K const & key,
+                                   Constructor c = Constructor())
+    {
+        static auto const createShared = [](type & sharedResourceMap,
+                                            K const & key,
+                                            ValueObj_ & valueObj)
+        {
+            std::shared_ptr<T> r(
+                valueObj.realPtr.get(),
+                [&sharedResourceMap, key](T * const) noexcept {
+                    std::lock_guard<std::mutex> const guard(
+                                sharedResourceMap.m_mutex);
+                    auto const it = sharedResourceMap.m_data.find(key);
+                    if (it != sharedResourceMap.m_data.end()) {
+                        ValueObj_ & obj = it->second;
+                        if (obj.weakPtr.expired()) {
+                            sharedResourceMap.m_data.erase(it);
+                            sharedResourceMap.m_cond.notify_all();
+                        }
+                    }
+                });
+            valueObj.weakPtr = r;
+            return r;
+        };
+
+        std::lock_guard<std::mutex> const guard(m_mutex);
+        auto it = m_data.find(key);
+        if (it != m_data.end()) {
+            ValueObj_ & obj = it->second;
+            if (auto s = obj.weakPtr.lock())
+                return s;
+            return createShared(*this, key, obj);
+        } else {
+            std::unique_ptr<T> realPtr(c());
+            using VT = typename decltype(m_data)::value_type;
+            auto rp = m_data.emplace(VT(key,
+                                        ValueObj_{{}, std::move(realPtr)}));
+            assert(rp.second);
+            m_cond.notify_all();
+            return createShared(*this, key, rp.first->second);
+        }
+    }
+
+private: /* Fields: */
+
+    mutable std::mutex m_mutex;
+    mutable std::condition_variable m_cond;
+    Map_ m_data;
+
+};
+
+} /* namespace Sharemind { */
+
+#endif /* SHAREMIND_MIN_H */
---
+++ tests/TestSharedResourceMap.cpp
@@ -0,0 +1,172 @@
+/*
+ * Copyright (C) 2015 Cybernetica
+ *
+ * Research/Commercial License Usage
+ * Licensees holding a valid Research License or Commercial License
+ * for the Software may use this file according to the written
+ * agreement between you and Cybernetica.
+ *
+ * GNU General Public License Usage
+ * Alternatively, this file may be used under the terms of the GNU
+ * General Public License version 3.0 as published by the Free Software
+ * Foundation and appearing in the file LICENSE.GPL included in the
+ * packaging of this file.  Please review the following information to
+ * ensure the GNU General Public License version 3.0 requirements will be
+ * met: http://www.gnu.org/copyleft/gpl-3.0.html.
+ *
+ * For further information, please contact us at sharemind@cyber.ee.
+ */
+
+#include "../src/SharedResourceMap.h"
+
+#include <atomic>
+#include <cassert>
+// #include <chrono>
+#include <condition_variable>
+#include <memory>
+#include <mutex>
+#include <thread>
+#include <type_traits>
+#include <vector>
+
+
+namespace {
+
+constexpr unsigned const numThreads = 100u;
+constexpr unsigned const numIters = 100000u;
+
+template <typename T>
+using NC = typename std::remove_const<T>::type;
+
+constexpr auto const expectedCount = (numThreads * numIters) / 2u;
+std::atomic<NC<decltype(expectedCount)> > count40(0u);
+std::atomic<NC<decltype(expectedCount)> > count42(0u);
+
+std::atomic<unsigned> whichNumber(0u);
+
+constexpr std::memory_order const relax = std::memory_order_relaxed;
+
+template <typename A>
+inline A inc(std::atomic<A> & v) noexcept
+{ return v.fetch_add(1u, relax); }
+
+struct SomeClass {
+    unsigned value;
+    void f(unsigned const expected, std::atomic<unsigned> & cnt) const noexcept {
+        assert(value == expected);
+        inc(cnt);
+    }
+};
+
+constexpr auto const maxConstructions = (numThreads * numIters);
+std::atomic<NC<decltype(maxConstructions)> > constructions(0u);
+
+auto const c40 = []{ inc(constructions); return new SomeClass{40u}; };
+auto const c42 = []{ inc(constructions); return new SomeClass{42u}; };
+
+std::mutex threadsAtStartMutex;
+std::condition_variable threadsAtStartCond;
+unsigned threadsAtStart = 0u;
+
+std::mutex startMutex;
+std::condition_variable startCond;
+bool start = false;
+bool fail = true;
+
+
+sharemind::SharedResourceMap<unsigned, SomeClass> map;
+
+} // anonymous namespace
+
+void threadFun() noexcept {
+    {
+        std::lock_guard<std::mutex> const guard(threadsAtStartMutex);
+        ++threadsAtStart;
+        threadsAtStartCond.notify_one();
+    }
+    {
+        std::unique_lock<std::mutex> lock(startMutex);
+        startCond.wait(lock, [&]() noexcept { return start; });
+        if (fail)
+            return;
+    }
+    try {
+        for (unsigned i = numIters; i > 0u; --i) {
+            bool const is40 = inc(whichNumber) % 2;
+            unsigned const number = is40 ? 40u : 42u;
+            auto & cnt = is40 ? count40 : count42;
+            auto const & constr = is40 ? c40 : c42;
+            auto v = map.getResource(number, constr);
+            assert(v);
+            v->f(number, cnt);
+        }
+    } catch (std::bad_alloc const &) {}
+}
+
+int main() {
+    // std::this_thread::sleep_for(std::chrono::seconds(2));
+    {
+        auto a = map.getResource(42u, c42);
+        static_assert(std::is_same<decltype(a),
+                                   std::shared_ptr<SomeClass> >::value, "");
+        assert(a);
+        assert(a->value == 42u);
+        auto b = map.getResource(42u, c42);
+        assert(b);
+        assert(b->value == 42u);
+        auto c = map.getResource(40u, c40);
+        assert(c);
+        assert(c->value == 40u);
+        assert(map.size() == 2u);
+        assert(a == b);
+        assert(a != c);
+        assert(b != c);
+        b.reset();
+        assert(map.size() == 2u);
+        a.reset();
+        assert(map.size() == 1u);
+    }
+    assert(map.empty());
+    std::vector<std::thread> threads;
+    threads.reserve(numThreads);
+    try {
+        for (unsigned i = 0u; i < numThreads; ++i)
+            threads.emplace_back(&threadFun);
+        {
+            std::unique_lock<std::mutex> lock(threadsAtStartMutex);
+            threadsAtStartCond.wait(
+                        lock,
+                        []() noexcept { return threadsAtStart == numThreads; });
+        }
+        {
+            std::lock_guard<std::mutex> const guard(startMutex);
+            start = true;
+            fail = false;
+            startCond.notify_all();
+        }
+    } catch (...) {
+        {
+            std::lock_guard<std::mutex> const guard(startMutex);
+            start = true;
+            startCond.notify_all();
+        }
+        while (!threads.empty()) {
+            (*threads.rbegin()).join();
+            threads.pop_back();
+        }
+        throw;
+    }
+    while (!threads.empty()) {
+        (*threads.rbegin()).join();
+        threads.pop_back();
+    }
+    assert(map.empty());
+    assert(count40.load(relax) == expectedCount);
+    assert(count42.load(relax) == expectedCount);
+
+    /// \warning This assertion will fail with very low probability:
+    assert(constructions.load(relax) < maxConstructions);
+
+    /// \todo Test forEach()
+    /// \todo Test waitForEmpty()
+}
