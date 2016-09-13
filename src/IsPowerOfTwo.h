0867e6752c7b0a74021acdb4206bec3ba7036a33
Jaak Ristioja <jaak.ristioja@cyber.ee>
2016-09-13 12:30:59 +0300

    Implemented isPowerOfTwo().

---
+++ src/IsPowerOfTwo.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2015 Cybernetica
+ *
+ * Research/Commercial License Usage
+ * Licensees holding a valid Research License or Commercial License
+ * for the Software may use this file according to the written
+ * agreement between you and Cybernetica.
+ *
+ * GNU General Public License Usage
+ * Alternatively, this file may be used under the terms of the GNU
+ * General Public License version 3.0 as published by the Free Software
+ * Foundation and appearing in the file LICENSE.GPL included in the
+ * packaging of this file.  Please review the following information to
+ * ensure the GNU General Public License version 3.0 requirements will be
+ * met: http://www.gnu.org/copyleft/gpl-3.0.html.
+ *
+ * For further information, please contact us at sharemind@cyber.ee.
+ */
+
+#ifndef SHAREMIND_ISPOWEROFTWO_H
+#define SHAREMIND_ISPOWEROFTWO_H
+
+#include <type_traits>
+
+
+namespace sharemind {
+
+template <typename T>
+inline constexpr bool isPowerOfTwo(T x) noexcept {
+    static_assert(std::is_unsigned<T>::value, "");
+    return (x != 0u) && ((x & (~x + 1u)) == x);
+}
+
+} /* namespace Sharemind { */
+
+#endif /* SHAREMIND_ISPOWEROFTWO_H */
