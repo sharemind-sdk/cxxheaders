e4a7c0b0a454087c4adb14649e63fcd75fe6e910
Jaak Ristioja <jaak.ristioja@cyber.ee>
2016-11-30 10:38:14 +0200

    CountMaxActor: Removed unused functionality, got rid of PartialStreamOperationException.

--- src/CountMaxActor.h
+++ src/CountMaxActor.h
@@ -27,7 +27,6 @@
 #include <type_traits>
 #include <utility>
 #include "FunctionTraits.h"
-#include "PartialStreamOperationException.h"
 
 
 namespace sharemind {
@@ -39,106 +38,27 @@ using ActorWantDataType =
             typename sharemind::FunctionTraits<Actor>
                     ::template argument<1u>::type>::type;
 
-template <typename Actor,
-          bool COUNT_PARTIAL = false,
-          bool isNoexcept_ =
-                noexcept(std::declval<Actor &>()(
-                            std::declval<ActorWantDataType<Actor> * const>(),
-                            std::declval<std::size_t const>()))>
-class CountMaxActorImpl {
+} /* namespace Detail { */
 
-public: /* Constants: */
 
-    static constexpr bool const isNoexcept = false;
 
-public: /* Types: */
-
-    using Exception = typename std::remove_reference<Actor>::type::Exception;
+template <typename Actor>
+class CountMaxActor {
 
 public: /* Methods: */
 
-    CountMaxActorImpl(
-            CountMaxActorImpl<Actor, COUNT_PARTIAL, isNoexcept_> const &)
-            = delete;
-    CountMaxActorImpl(CountMaxActorImpl<Actor, COUNT_PARTIAL, isNoexcept_> &&)
-            = default;
-    CountMaxActorImpl<Actor, COUNT_PARTIAL, isNoexcept_> & operator=(
-            CountMaxActorImpl<Actor, COUNT_PARTIAL, isNoexcept_> &&) = default;
-    CountMaxActorImpl<Actor, COUNT_PARTIAL, isNoexcept_> & operator=(
-            CountMaxActorImpl<Actor, COUNT_PARTIAL, isNoexcept_> const &)
-            = delete;
+    CountMaxActor(CountMaxActor &&) = default;
+    CountMaxActor(CountMaxActor const &) = delete;
+    CountMaxActor & operator=(CountMaxActor &&) = default;
+    CountMaxActor & operator=(CountMaxActor const &) = delete;
 
     template <typename ... Args>
-    inline CountMaxActorImpl(Args && ... args)
+    inline CountMaxActor(Args && ... args)
             noexcept(noexcept(Actor(std::forward<Args>(args)...)))
         : m_actor(std::forward<Args>(args)...)
     {}
 
-    inline std::size_t operator()(ActorWantDataType<Actor> * const data,
-                                  std::size_t const size)
-    {
-        assert(data);
-        assert(size > 0u);
-        std::size_t const maxTransfer =
-                std::numeric_limits<std::size_t>::max() - m_count;
-        if (maxTransfer == 0u)
-            return 0u;
-        std::size_t const canTransfer = std::min(size, maxTransfer);
-        try {
-            std::size_t const transferred = m_actor(data, canTransfer);
-            assert(transferred <= canTransfer);
-            m_count += transferred;
-            return transferred;
-        } catch (PartialStreamOperationException const & e) {
-            assert(e.size() < canTransfer);
-            if (COUNT_PARTIAL)
-                m_count += e.size();
-            throw;
-        } catch (Exception &) {
-            throw;
-        } catch (...) {
-            std::unexpected();
-        }
-    }
-
-    inline std::size_t count() const noexcept { return m_count; }
-
-private: /* Fields: */
-
-    Actor m_actor;
-    std::size_t m_count{0u};
-
-};
-
-template <typename Actor, bool COUNT_PARTIAL>
-class CountMaxActorImpl<Actor, COUNT_PARTIAL, true> {
-
-public: /* Constants: */
-
-    static constexpr bool const isNoexcept = true;
-
-public: /* Types: */
-
-    using Exception = typename std::remove_reference<Actor>::type::Exception;
-
-public: /* Methods: */
-
-    CountMaxActorImpl(CountMaxActorImpl<Actor, COUNT_PARTIAL, true> &&)
-            = default;
-    CountMaxActorImpl(CountMaxActorImpl<Actor, COUNT_PARTIAL, true> const &)
-            = delete;
-    CountMaxActorImpl<Actor, COUNT_PARTIAL, true> & operator=(
-            CountMaxActorImpl<Actor, COUNT_PARTIAL, true> &&) = default;
-    CountMaxActorImpl<Actor, COUNT_PARTIAL, true> & operator=(
-            CountMaxActorImpl<Actor, COUNT_PARTIAL, true> const &) = delete;
-
-    template <typename ... Args>
-    inline CountMaxActorImpl(Args && ... args)
-            noexcept(noexcept(Actor(std::forward<Args>(args)...)))
-        : m_actor(std::forward<Args>(args)...)
-    {}
-
-    inline std::size_t operator()(ActorWantDataType<Actor> * const data,
+    inline std::size_t operator()(Detail::ActorWantDataType<Actor> * const data,
                                   std::size_t const size) noexcept
     {
         assert(data);
@@ -162,11 +82,6 @@ private: /* Fields: */
     std::size_t m_count{0u};
 
 };
-
-} /* namespace Detail { */
-
-template <typename Actor, bool COUNT_PARTIAL = false>
-using CountMaxActor = Detail::CountMaxActorImpl<Actor, COUNT_PARTIAL>;
 
 } /* namespace sharemind { */
 
