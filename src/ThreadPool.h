b30f26cb7d3d907492db19ef827187094b522866
Jaak Ristioja <jaak.ristioja@cyber.ee>
2017-01-17 15:20:26 +0200

    ThreadPool: Optimized away some move-constructions.

--- src/ThreadPool.h
+++ src/ThreadPool.h
@@ -286,24 +286,24 @@ public: /* Methods: */
     }
 
     template <typename F>
-    static inline Task createTask(F f) {
+    static inline Task createTask(F && f) {
         struct CustomTask: TaskBase {
-            inline CustomTask(F f) : m_f{std::move(f)} {}
+            inline CustomTask(F && f) : m_f(std::move(f)) {}
             inline void operator()(Task && task) noexcept final override
             { m_f(std::move(task)); }
             F m_f;
         };
-        return createTask(new CustomTask{std::move(f)});
+        return createTask(new CustomTask(std::move(f)));
     }
 
     template <typename F>
-    static inline Task createSimpleTask(F f) {
+    static inline Task createSimpleTask(F && f) {
         struct CustomSimpleTask: TaskBase {
-            inline CustomSimpleTask(F f) : m_f{std::move(f)} {}
+            inline CustomSimpleTask(F && f) : m_f(std::move(f)) {}
             inline void operator()(Task &&) noexcept final override { m_f(); }
             F m_f;
         };
-        return createTask(new CustomSimpleTask{std::move(f)});
+        return createTask(new CustomSimpleTask(std::move(f)));
     }
 
     inline void submit(Task task) noexcept {
