d2966f16f6d404636a9cda4442ca1c8f3fc79cfb
Jaak Ristioja <jaak.ristioja@cyber.ee>
2017-06-12 15:50:06 +0300

    TemplateFirstType: Work around GCC PR 59498 / C++ DR 1430.

--- src/TemplateFirstType.h
+++ src/TemplateFirstType.h
@@ -22,7 +22,30 @@
 
 namespace sharemind {
 
-template <typename T, typename ...> using TemplateFirstType = T;
+/*
+  TemplateFirstType was previously defined as
+
+    template <typename T, typename ...> using TemplateFirstType = T;
+
+  But this caused problems with some compilers, e.g. "pack expansion argument
+  for non-pack parameter" errors. To work around these issues we needed to add
+  another level of indirection. See GCC PR 59498 [1] and C++ DR 1430 [2] for
+  details.
+
+  [1]: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=59498
+  [2]: http://open-std.org/jtc1/sc22/wg21/docs/cwg_active.html#1430
+*/
+
+namespace Detail {
+namespace TemplateFirstType {
+
+template <typename T, typename ...> struct Impl { using type = T; };
+
+} /* namespace TemplateFirstType { */
+} /* namespace Detail { */
+
+template <typename ... Ts>
+using TemplateFirstType = typename Detail::TemplateFirstType::Impl<Ts...>::type;
 
 } /* namespace sharemind { */
 
