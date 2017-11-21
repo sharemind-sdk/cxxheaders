2390ac9485e1e42a56eb8a67799818f731f5248b
Jaak Ristioja <jaak.ristioja@cyber.ee>
2017-11-21 16:07:34 +0200

    tests, TestThrowWithNested: Workaround GCC PR 62154.

--- tests/TestThrowWithNested.cpp
+++ tests/TestThrowWithNested.cpp
@@ -19,43 +19,68 @@
 
 #include "../src/ThrowWithNested.h"
 
+#include <sharemind/compiler-support/GccVersion.h>
 #include "../src/TestAssert.h"
 
 
 using sharemind::throwWithNested;
 
+/* Workaround GCC PR 62154 (https://gcc.gnu.org/bugzilla/show_bug.cgi?id=62154):
+
+  Sample error message:
+
+ /usr/include/c++/4.9/bits/nested_exception.h:90:59: error: cannot dynamic_cast
+        ‘& __ex’ (of type ‘const struct A*’) to type ‘const class
+        std::nested_exception*’ (source type is not polymorphic)
+       { return dynamic_cast<const nested_exception*>(&__ex); }
+                                                           ^
+
+*/
+#if !defined(SHAREMIND_GCC_VERSION) || SHAREMIND_GCC_VERSION >= 50000
+using FirstException = int;
 struct A {};
 struct B {};
 struct C {};
+#else
+struct FirstException {
+    FirstException(int v) noexcept : m_v(v) {}
+    virtual ~FirstException() noexcept {}
+    bool operator==(int v) const noexcept { return m_v == v; }
+    int m_v;
+};
+struct A { virtual ~A() noexcept {} };
+struct B { virtual ~B() noexcept {} };
+struct C { virtual ~C() noexcept {} };
+#endif
 
 int main() {
     try {
         try {
-            throw 42;
+            throw FirstException(42);
         } catch (...) {
             throwWithNested();
         }
-    } catch (int const e) {
+    } catch (FirstException const & e) {
         SHAREMIND_TESTASSERT(e == 42);
     } catch (...) { SHAREMIND_TEST_UNREACHABLE; }
 
     try {
         try {
-            throw 42;
+            throw FirstException(42);
         } catch (...) {
             throwWithNested(A());
         }
     } catch (A const & e) {
         try {
             std::rethrow_if_nested(e);
-        } catch (int const e) {
+        } catch (FirstException const & e) {
             SHAREMIND_TESTASSERT(e == 42);
         } catch (...) { SHAREMIND_TEST_UNREACHABLE; }
     } catch (...) { SHAREMIND_TEST_UNREACHABLE; }
 
     try {
         try {
-            throw 42;
+            throw FirstException(42);
         } catch (...) {
             throwWithNested(A(), B());
         }
@@ -65,7 +90,7 @@ int main() {
         } catch (A const & e) {
             try {
                 std::rethrow_if_nested(e);
-            } catch (int const e) {
+            } catch (FirstException const & e) {
                 SHAREMIND_TESTASSERT(e == 42);
             } catch (...) { SHAREMIND_TEST_UNREACHABLE; }
         } catch (...) { SHAREMIND_TEST_UNREACHABLE; }
@@ -73,7 +98,7 @@ int main() {
 
     try {
         try {
-            throw 42;
+            throw FirstException(42);
         } catch (...) {
             throwWithNested(A(), B(), C());
         }
@@ -86,7 +111,7 @@ int main() {
             } catch (A const & e) {
                 try {
                     std::rethrow_if_nested(e);
-                } catch (int const e) {
+                } catch (FirstException const & e) {
                     SHAREMIND_TESTASSERT(e == 42);
                 } catch (...) { SHAREMIND_TEST_UNREACHABLE; }
             } catch (...) { SHAREMIND_TEST_UNREACHABLE; }
