148725a5110c4ecc70edffdd82e8b63723bd3c28
Jaak Ristioja <jaak.ristioja@cyber.ee>
2019-01-24 16:45:11 +0200

    Implemented means to search the call stack for injectable information.

---
+++ src/CallStack.h
@@ -0,0 +1,185 @@
+/*
+ * Copyright (C) Cybernetica
+ *
+ * Research/Commercial License Usage
+ * Licensees holding a valid Research License or Commercial License
+ * for the Software may use this file according to the written
+ * agreement between you and Cybernetica.
+ *
+ * GNU General Public License Usage
+ * Alternatively, this file may be used under the terms of the GNU
+ * General Public License version 3.0 as published by the Free Software
+ * Foundation and appearing in the file LICENSE.GPL included in the
+ * packaging of this file.  Please review the following information to
+ * ensure the GNU General Public License version 3.0 requirements will be
+ * met: http://www.gnu.org/copyleft/gpl-3.0.html.
+ *
+ * For further information, please contact us at sharemind@cyber.ee.
+ */
+
+#ifndef SHAREMIND_CALLSTACK_H
+#define SHAREMIND_CALLSTACK_H
+
+#include <type_traits>
+#include <utility>
+
+
+namespace sharemind {
+
+template <typename Key, typename Value = void>
+class CallStack {
+
+public: /* Types: */
+
+    using KeyType = Key;
+    using ValueType = Value;
+
+    class Context {
+
+        friend class CallStack;
+
+    public: /* Types: */
+
+        using KeyType = Key;
+        using ValueType = Value;
+
+    public: /* Methods: */
+
+        Context(Context &&) = delete;
+        Context(Context const &) = delete;
+
+        template <typename Key_>
+        explicit Context(Key_ && key)
+                noexcept(std::is_nothrow_constructible<Key, Key_ &&>::value
+                         && std::is_nothrow_default_constructible<Value>::value)
+            : m_key(std::forward<Key_>(key))
+        { CallStack::m_topContext = this; }
+
+        template <typename Key_, typename Value_>
+        Context(Key_ && key, Value_ && value)
+                noexcept(std::is_nothrow_constructible<Key, Key_ &&>::value
+                         && std::is_nothrow_constructible<Value,
+                                                          Value_ &&>::value)
+            : m_key(std::forward<Key_>(key))
+            , m_value(std::forward<Value_>(value))
+        { CallStack::m_topContext = this; }
+
+
+        ~Context() noexcept { CallStack::m_topContext = m_next; }
+
+
+        Context & operator=(Context &&) = delete;
+        Context & operator=(Context const &) = delete;
+
+
+        Key & key() noexcept { return m_key; }
+        Key const & key() const noexcept { return m_key; }
+
+        Value & value() noexcept { return m_value; }
+        Value const & value() const noexcept { return m_value; }
+
+        Context * next() const noexcept { return m_next; }
+
+    private: /* Fields: */
+
+        Key m_key;
+        Value m_value;
+        Context * const m_next = CallStack::m_topContext;
+
+    };
+    friend class Context;
+
+public: /* Methods: */
+
+    static Context * top() noexcept { return m_topContext; }
+
+    template <typename Key_>
+    static Context * contains(Key_ && key) noexcept {
+        for (auto c = m_topContext; c != nullptr; c = c->m_next)
+            if (c->m_key == key)
+                return c;
+        return nullptr;
+    }
+
+private: /* Fields: */
+
+    static thread_local Context * m_topContext;
+
+};
+
+template <typename Key, typename Value>
+thread_local typename CallStack<Key, Value>::Context *
+CallStack<Key, Value>::m_topContext;
+
+template <typename Key>
+class CallStack<Key, void> {
+
+public: /* Types: */
+
+    using KeyType = Key;
+
+    class Context {
+
+        friend class CallStack;
+
+    public: /* Types: */
+
+        using KeyType = Key;
+
+    public: /* Methods: */
+
+        Context(Context &&) = delete;
+        Context(Context const &) = delete;
+
+        template <typename Key_>
+        explicit Context(Key_ && key)
+                noexcept(std::is_nothrow_constructible<Key, Key_ &&>::value)
+            : m_key(std::forward<Key_>(key))
+        { CallStack::m_topContext = this; }
+
+
+        ~Context() noexcept { CallStack::m_topContext = m_next; }
+
+
+        Context & operator=(Context &&) = delete;
+        Context & operator=(Context const &) = delete;
+
+
+        Key & key() noexcept { return m_key; }
+        Key const & key() const noexcept { return m_key; }
+
+        Context * next() const noexcept { return m_next; }
+
+    private: /* Fields: */
+
+        Key m_key;
+        Context * const m_next = CallStack::m_topContext;
+
+    };
+    friend class Context;
+
+public: /* Methods: */
+
+    static Context * top() noexcept { return m_topContext; }
+
+    template <typename Key_>
+    static Context * contains(Key_ && key) noexcept {
+        for (auto c = m_topContext; c != nullptr; c = c->m_next)
+            if (c->m_key == key)
+                return c;
+        return nullptr;
+    }
+
+private: /* Fields: */
+
+    static thread_local Context * m_topContext;
+
+};
+
+template <typename Key>
+thread_local typename CallStack<Key, void>::Context *
+CallStack<Key, void>::m_topContext;
+
+} /* namespace Sharemind { */
+
+#endif /* SHAREMIND_CALLSTACK_H */
---
+++ tests/TestCallStack.cpp
@@ -0,0 +1,116 @@
+/*
+ * Copyright (C) Cybernetica
+ *
+ * Research/Commercial License Usage
+ * Licensees holding a valid Research License or Commercial License
+ * for the Software may use this file according to the written
+ * agreement between you and Cybernetica.
+ *
+ * GNU General Public License Usage
+ * Alternatively, this file may be used under the terms of the GNU
+ * General Public License version 3.0 as published by the Free Software
+ * Foundation and appearing in the file LICENSE.GPL included in the
+ * packaging of this file.  Please review the following information to
+ * ensure the GNU General Public License version 3.0 requirements will be
+ * met: http://www.gnu.org/copyleft/gpl-3.0.html.
+ *
+ * For further information, please contact us at sharemind@cyber.ee.
+ */
+
+#include "../src/CallStack.h"
+
+#include <type_traits>
+#include "../src/TestAssert.h"
+
+
+using sharemind::CallStack;
+
+struct Key { unsigned value; };
+struct Value { unsigned value; };
+
+bool operator==(Key const & lhs, unsigned rhs) noexcept
+{ return lhs.value == rhs; }
+
+bool operator==(Value const & lhs, unsigned rhs) noexcept
+{ return lhs.value == rhs; }
+
+#define SA(...) static_assert(__VA_ARGS__, "")
+SA(std::is_same<typename CallStack<Key>::KeyType, Key>::value);
+SA(std::is_same<typename CallStack<Key>::Context::KeyType, Key>::value);
+SA(std::is_same<typename CallStack<Key, Value>::KeyType, Key>::value);
+SA(std::is_same<typename CallStack<Key, Value>::Context::KeyType, Key>::value);
+SA(std::is_same<typename CallStack<Key, Value>::ValueType, Value>::value);
+SA(std::is_same<typename CallStack<Key, Value>::Context::ValueType, Value>::value);
+
+constexpr unsigned const maxLevels = 24u;
+
+void testWithoutValue(unsigned level = 0u) {
+    using CS = CallStack<Key>;
+    auto oldTop = CS::top();
+    if (level > 0u) {
+        auto context = oldTop;
+        SHAREMIND_TESTASSERT(context != nullptr);
+        for (unsigned expected = level - 1u;; --expected) {
+            SHAREMIND_TESTASSERT(context->key().value == expected);
+            SHAREMIND_TESTASSERT(CS::contains(expected) == context);
+            {
+                CS::Context const overrideContext(Key{expected});
+                SHAREMIND_TESTASSERT(CS::top() == &overrideContext);
+                SHAREMIND_TESTASSERT(CS::contains(expected)
+                                     == &overrideContext);
+            }
+            SHAREMIND_TESTASSERT(CS::top() == oldTop);
+            context = context->next();
+            if (expected == 0u)
+                break;
+        }
+        SHAREMIND_TESTASSERT(context == nullptr);
+    }
+    SHAREMIND_TESTASSERT(CS::contains(level) == nullptr);
+    CS::Context const context(Key{level});
+    SHAREMIND_TESTASSERT(context.key().value == level);
+    SHAREMIND_TESTASSERT(context.next() == oldTop);
+    SHAREMIND_TESTASSERT(CS::contains(level) == &context);
+    SHAREMIND_TESTASSERT(CS::top() == &context);
+    if (level < maxLevels)
+        testWithoutValue(level + 1u);
+}
+
+void testWithValue(unsigned level = 0u) {
+    using CS = CallStack<Key, Value>;
+    auto oldTop = CS::top();
+    if (level > 0u) {
+        auto context = oldTop;
+        SHAREMIND_TESTASSERT(context != nullptr);
+        for (unsigned expected = level - 1u;; --expected) {
+            SHAREMIND_TESTASSERT(context->key().value == expected);
+            SHAREMIND_TESTASSERT(context->value().value == expected + 42u);
+            SHAREMIND_TESTASSERT(CS::contains(expected) == context);
+            {
+                CS::Context const overrideContext(Key{expected}, Value{});
+                SHAREMIND_TESTASSERT(CS::top() == &overrideContext);
+                SHAREMIND_TESTASSERT(CS::contains(expected)
+                                     == &overrideContext);
+            }
+            SHAREMIND_TESTASSERT(CS::top() == oldTop);
+            context = context->next();
+            if (expected == 0u)
+                break;
+        }
+        SHAREMIND_TESTASSERT(context == nullptr);
+    }
+    SHAREMIND_TESTASSERT(CS::contains(level) == nullptr);
+    CS::Context const context(Key{level}, Value{level + 42u});
+    SHAREMIND_TESTASSERT(context.key().value == level);
+    SHAREMIND_TESTASSERT(context.value().value == level + 42u);
+    SHAREMIND_TESTASSERT(context.next() == oldTop);
+    SHAREMIND_TESTASSERT(CS::contains(level) == &context);
+    SHAREMIND_TESTASSERT(CS::top() == &context);
+    if (level < maxLevels)
+        testWithValue(level + 1u);
+}
+
+int main() {
+    testWithoutValue();
+    testWithValue();
+}
